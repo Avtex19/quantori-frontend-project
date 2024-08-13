59e0ae9e9d95ac01bfbb82989e4b05a5108d7280
nickgabriadze <atukaberadze19@gmail.com>
2024-08-13 18:48:22 +0400

    added some css for the user's profile information

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,37 +1,93 @@
 import { useState } from "react";
+import useRetrieveUser from "../hooks/useRetrieveUser";
+import useTokenExpiry from "../hooks/useTokenExpiry";
 
 export default function Body() {
-  const [userEmail, setUserEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [userPassword, setUserPassword] = useState("");
 
-  const [userToken, setUserToken] = useState("");
+  const [userData, setUserData] = useRetrieveUser();
+  const [loading, setLoading] = useState(false);
+  useTokenExpiry(localStorage.getItem("token"));
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
+    fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        password: userPassword,
+        expiresInMins: 1,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+        setUserData(data);
+        setLoading(false);
+        setUsername("");
+        setUserPassword("");
+      });
   };
 
-  return (
+  return userData === null ? (
     <div className="form-wrapper">
-      <form className="login-form">
+      <form onSubmit={handleSubmit} className="login-form">
         <h2>Login</h2>
 
         <div className="inputs-container">
           <div className="input-label-wrapper">
-            <label>Email</label>
-            <input type="email" placeholder="email@example.com" required />
+            <label>Username</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
           </div>
 
           <div className="input-label-wrapper">
             <label>Password</label>
-            <input type="password" required />
+            <input
+              value={userPassword}
+              onChange={(e) => setUserPassword(e.target.value)}
+              type="password"
+              required
+            />
           </div>
         </div>
 
         <div className="buttons">
-          <button className="cancel">Cancel</button>
-          <button className="login">Login</button>
+          <button type="reset" className="cancel">
+            Cancel
+          </button>
+          <button className="login">{loading ? 'Loading...' : 'Login'}</button>
         </div>
       </form>
+    </div>
+  ) : (
+    <div className="user-profile">
+      <h4>
+        Email: <span>{userData.email} </span>
+      </h4>
+      <h4>
+        username: <span>{userData.username}</span>
+      </h4>
+      <h4>
+        Gender: <span>{userData.gender}</span>
+      </h4>
+
+      <button
+        onClick={() => {
+          setUserData(null);
+          localStorage.clear();
+        }}
+        className="login"
+      >
+        Log Out
+      </button>
     </div>
   );
 }
