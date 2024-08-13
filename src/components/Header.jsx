ef170d20f405a0076a7cbc1635c573caff479421
nickgabriadze <atukaberadze19@gmail.com>
2024-08-13 16:19:40 +0400

    Updated file type

---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./../App.css";
+import AccountSymbol from "./../assets/account-icon.svg";
+
+export default function Header() {
+  const [nav, setNav] = useState("home");
+
+  return (
+    <div className="header-wrapper">
+      <div className="navigation">
+        <div className="user-icon">
+          <img src={AccountSymbol} width={45} alt="Account icon" />
+        </div>
+        <nav>
+          <a
+            onClick={() => setNav("home")}
+            className="nav-home"
+            style={
+              nav === "home"
+                ? { backgroundColor: "#48752c", color: "white" }
+                : { backgroundColor: "white", color: "#48752c" }
+            }
+          >
+            Home
+          </a>
+          <a
+            onClick={() => setNav("contact")}
+            className="nav-contact"
+            style={
+              nav === "contact"
+                ? { backgroundColor: "#48752c", color: "white" }
+                : { backgroundColor: "white", color: "#48752c" }
+            }
+          >
+            Contact
+          </a>
+          <a
+            onClick={() => setNav("about")}
+            className="nav-about"
+            style={
+              nav === "about"
+                ? { backgroundColor: "#48752c", color: "white" }
+                : { backgroundColor: "white", color: "#48752c" }
+            }
+          >
+            About
+          </a>
+        </nav>
+      </div>
+
+      <div>
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
